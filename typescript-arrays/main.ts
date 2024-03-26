8ee5d6d74f84152ba246d66669e1b0def92b48ae
Vanessa Viray <13virayvanessab@gmail.com>
2024-03-26 19:57:26 +0000

    Completed typescript-arrays exercise.

---
+++ typescript-arrays/main.ts
@@ -0,0 +1,27 @@
+const colors: string[] = ['red', 'white', 'blue'];
+
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+
+console.log(
+  'America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]
+);
+
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+
+console.log('value of colors:', colors);
+console.log(typeof colors);
+
+const students: string[] = ['Ryan', 'Jo', 'Cody', 'Vanessa'];
+const numberOfStudents: number = students.length;
+console.log(`There are ${numberOfStudents} students in the class.`);
+
+const lastIndex: number = numberOfStudents - 1;
+const lastStudent: string = students[lastIndex];
+console.log(`The last student in the array is ${lastStudent}.`);
+
+console.log('value of students:', students);
+console.log(typeof students);
