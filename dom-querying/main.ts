33760c4e6fbf9c3da513ae3fd4ae6f8aa4461176
Vanessa Viray <13virayvanessab@gmail.com>
2024-04-09 01:22:36 +0000

    Edited variable names.

--- dom-querying/main.ts
+++ dom-querying/main.ts
@@ -1,18 +1,17 @@
-const h1QueryResult = document.querySelector('h1');
-console.log(h1QueryResult);
-console.dir(h1QueryResult);
+const $h1 = document.querySelector('h1');
+console.log($h1);
+console.dir($h1);
 
-const idExplanationQueryResult = document.querySelector('#explanation');
-console.log(idExplanationQueryResult);
-console.dir(idExplanationQueryResult);
+const $explanation = document.querySelector('#explanation');
+console.log($explanation);
+console.dir($explanation);
 
-const classHintQueryResult = document.querySelector('.hint');
-console.log(classHintQueryResult);
-console.dir(classHintQueryResult);
+const $hint = document.querySelector('.hint');
+console.log($hint);
+console.dir($hint);
 
-const elementPQueryAllResults = document.querySelectorAll('p');
-console.log(elementPQueryAllResults);
+const $p = document.querySelectorAll('p');
+console.log($p);
 
-const classExampleLinkQueryAllResults =
-  document.querySelectorAll('.example-link');
-console.log(classExampleLinkQueryAllResults);
+const $exampleLink = document.querySelectorAll('.example-link');
+console.log($exampleLink);
