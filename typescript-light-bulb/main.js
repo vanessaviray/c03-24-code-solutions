760a09917579db818804be7b5dc0d80a25a8a7ae
Vanessa Viray <13virayvanessab@gmail.com>
2024-04-10 00:14:56 +0000

    Completed typescript-light-bulb challenge.

---
+++ typescript-light-bulb/main.js
@@ -0,0 +1,20 @@
+'use strict';
+let toggleCount = 0;
+const toggle = document.querySelector('.circle');
+const toggleShadow = document.querySelector('.circle-border');
+const toggleBackground = document.querySelector('.container');
+if (!toggle) throw new Error('The .circle query failed');
+if (!toggleShadow) throw new Error('The .circle-border query failed');
+if (!toggleBackground) throw new Error('The .container query failed');
+toggle.addEventListener('click', () => {
+  toggleCount++;
+  if (toggleCount % 2 === 0) {
+    toggle.className = 'circle';
+    toggleShadow.className = 'circle-border';
+    toggleBackground.className = 'container';
+  } else {
+    toggle.className = 'circle off';
+    toggleShadow.className = 'circle-border off';
+    toggleBackground.className = 'container off';
+  }
+});
