2c2378265b28fc69205b7761548f5f6f3c2f3b52
Vanessa Viray <13virayvanessab@gmail.com>
2024-05-22 15:47:29 +0000

    Completed react-multiple-pages exercise.

---
+++ react-multiple-pages/my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { About } from './pages/About';
+import { Dashboard } from './pages/Dashboard';
+import { Header } from './components/Header';
+import { NotFound } from './pages/NotFound';
+import { Details } from './pages/Details';
+import { Routes, Route } from 'react-router-dom';
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Dashboard />} />
+        <Route path="details/:itemId" element={<Details />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
---
+++ react-multiple-pages/my-app/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+export function NotFound() {
+  return (
+    <div className="flex">
+      <div className="flex-1 py-12 text-center">
+        <h3>Uh oh, we could not find the page you were looking for!</h3>
+        <div className="text-gray-700 cursor-pointer">
+          Return to the Dashboard
+        </div>
+      </div>
+    </div>
+  );
+}
