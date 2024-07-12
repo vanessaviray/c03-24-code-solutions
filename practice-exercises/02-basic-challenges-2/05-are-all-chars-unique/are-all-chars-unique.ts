8b081f352b02edb269b7347e6e8af317ffb2f4d2
Vanessa Viray <13virayvanessab@gmail.com>
2024-07-12 18:21:13 +0000

    Completed 02-basic-challenges-2 challenge 5.

--- practice-exercises/02-basic-challenges-2/05-are-all-chars-unique/are-all-chars-unique.ts
+++ practice-exercises/02-basic-challenges-2/05-are-all-chars-unique/are-all-chars-unique.ts
@@ -1,3 +1,13 @@
 export function areAllCharactersUnique(str: string): boolean {
-  return false;
+  const charCount: Record<string, boolean> = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char]) {
+      return false;
+    }
+    charCount[char] = true;
+  }
+
+  return true;
 }
