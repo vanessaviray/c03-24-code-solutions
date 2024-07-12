044bad4aa5d8e0730adb02c778ab4d3134e77cc0
Vanessa Viray <13virayvanessab@gmail.com>
2024-07-12 17:03:43 +0000

    02-basic-challenges-2 challenges 1-3.

--- practice-exercises/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
+++ practice-exercises/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
@@ -1,3 +1,15 @@
 export function findMissingNumber(arr: number[]): number | undefined {
-  return undefined;
+  if (!arr || arr.length === 0) {
+    return undefined;
+  }
+
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    actualSum += arr[i];
+  }
+
+  return expectedSum - actualSum;
 }
